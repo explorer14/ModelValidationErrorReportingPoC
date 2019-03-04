b0f763667093bf83393f4cc90714b85fbd439aa3
Aman Agrawal <amanag1983@gmail.com>
2019-03-04 12:14:16 +0100

    Add Serilog

--- ASPNETCore2_0/Startup.cs
+++ ASPNETCore2_0/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 
 namespace ASPNETCore2_0
 {
@@ -16,17 +17,23 @@ namespace ASPNETCore2_0
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(x => x.Filters.Add(new ModelBindingFailureFilterSimple()));
+            services.AddMvc(x => x.Filters.Add(new ModelBindingFailureFilterSimple(Log.Logger)));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            ConfigureLogging();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseMvc();
+        }
+        
+        private void ConfigureLogging()
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
         }
     }
 }
